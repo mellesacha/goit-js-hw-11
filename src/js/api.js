8c0b415d9f536b74572fecef23961b9c372bc84b
mellesacha <0251817@gmail.com>
2023-06-23 16:30:12 +0400

    hw11-3

--- src/js/api.js
+++ src/js/api.js
@@ -3,50 +3,63 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const API_KEY = '37603150-cf2f7acce1783634ed18c2efe';
 const URL_BASE = 'https://pixabay.com/api/';
-const gallery = document.querySelector('.gallery');
-const btnLoadMore = document.querySelector('.load-more');
 
-btnLoadMore.style.display = "none";
- 
-function getImage(query) {
+// export default class SearchImageApi{
 
-    axios.get(`${URL_BASE}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`).then(onMarkup)
-};
+//     constructor() {
 
-function onMarkup(obj) {
-    const pictureObj = obj.data.hits;
+//     }
 
-    if (pictureObj.length === 0) {
-        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    function getImage(query, page) {
+    // axios.get(`${URL_BASE}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=3&page=${page}`).then(onMarkup)
+        return axios.get(`${URL_BASE}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=200&page=${page}`)
     }
-        pictureObj.map(e => gallery.insertAdjacentHTML("beforeend", OnMarkupCard(e)))
-};
+
+ 
+// function getImage(query,page) {
+
+//     axios.get(`${URL_BASE}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=3&page=${page}`).then(onMarkup);
+    
+// };
+
+// function onMarkup(obj) {
+//     const pictureObj = obj.data.hits;
+
+//     if (pictureObj.length === 0) {
+//         Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+//     }
+
+//     else {
+//         pictureObj.map(e => gallery.insertAdjacentHTML("beforeend", OnMarkupCard(e)))
+//         btnLoadMore.classList.remove('is-hidden')
+//     }
+// };
 
 
-function OnMarkupCard(e) {
-       return `<div class="photo-card">
-  <img src="${e.webformatURL}" alt="${e.tags}" width="300px" height="180px" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <span>${e.likes}</span>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-       <span>${e.views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-       <span>${e.comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-       <span>${e.downloads}</span>
-    </p>
-  </div>
-</div>`
+// function OnMarkupCard(e) {
+//        return `<div class="photo-card">
+//   <img src="${e.webformatURL}" alt="${e.tags}" width="300px" height="180px" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//       <span>${e.likes}</span>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//        <span>${e.views}</span>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//        <span>${e.comments}</span>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//        <span>${e.downloads}</span>
+//     </p>
+//   </div>
+// </div>`
      
-};
+// };
 
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,86 @@
 import getImage from './api.js'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// import SearchImageApi from './api.js'
 
 const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
+
 
 form.addEventListener('submit', onSubmit);
+
+// const searchImageApi = new SearchImageApi();
+
+let page = 1;
 
 function onSubmit(e) {
     e.preventDefault();
 
     const query = form.elements.searchQuery.value;
+    gallery.innerHTML = '';
+    btnLoadMore.classList.add('is-hidden')
 
     if (query.trim() === '') {
         return
     }
-    getImage(query);
+    getImage(query, page).then(onMarkup).catch(console.log('a'));
 
+    btnLoadMore.addEventListener('click', onClickBtn);
+
+    function onClickBtn() {
+        page += 1;
+        getImage(query, page).then(onMarkup).catch(console.log('n'))
+        // console.log(getImage(query, page).then(b))
+    }
     e.currentTarget.reset();
+}
+
+// function b(obj) {
+//     return console.log(obj.data.totalHits) 
+// }
+    
+
+
+
+
+function onMarkup(obj) {
+    const pictureObj = obj.data.hits;
+
+    if (pictureObj.length === 0) {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+
+    else {
+        pictureObj.map(e => gallery.insertAdjacentHTML("beforeend", OnMarkupCard(e)))
+        btnLoadMore.classList.remove('is-hidden')
+    }
 };
 
 
+function OnMarkupCard(e) {
+       return `<div class="photo-card">
+  <img src="${e.webformatURL}" alt="${e.tags}" width="300px" height="180px" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span>${e.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+       <span>${e.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+       <span>${e.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+       <span>${e.downloads}</span>
+    </p>
+  </div>
+</div>`
+     
+};
 
  
